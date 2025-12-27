f2ed00f447f70d85aa59b86fc8e015f72c3fac61
Karan Rajput <r.karan21011@gmail.com>
2025-12-27 22:04:42 +0530

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'bootswatch/dist/Lumen/bootstrap.min.css';
+import 'bootswatch/dist/lumen/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createRoot(document.getElementById('root')).render(
